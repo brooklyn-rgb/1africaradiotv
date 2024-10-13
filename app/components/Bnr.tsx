c72de82c59f09991cc5f59a02f36d4527967ed78
brooklyn-rgb <brooklynstore151@gmail.com>
2024-10-13 18:20:42 +0200

    first commit

--- app/components/Bnr.tsx
+++ app/components/Bnr.tsx
@@ -8,8 +8,8 @@ const Bnr = () => {
   
   <div className="w-full relative flex items-center justify-center">
     <img src="/images/collen.jpg" alt="dining" className="w-full h-full absolute z-0 hidden xl:block" />
-    <img src="https://i.ibb.co/bbS3J9C/pexels-max-vakhtbovych-6301182-1.png" alt="dining" className="w-full h-full absolute z-0 hidden sm:block xl:hidden" />
-    <img src="https://i.ibb.co/JKkzGDs/pexels-max-vakhtbovych-6301182-1.png" alt="dining" className="w-full h-full absolute z-0 sm:hidden" />
+    <img src="/images/collen.jpg" alt="dining" className="w-full h-full absolute z-0 hidden sm:block xl:hidden" />
+    <img src="/images/collen.jpg" alt="dining" className="w-full h-full absolute z-0 sm:hidden" />
     <div className="bg-gray-800 bg-opacity-80 md:my-16 lg:py-16 py-10 w-full md:mx-24 md:px-12 px-4 flex flex-col items-center justify-center relative z-40">
       <h1 className="text-4xl font-semibold leading-9 text-white text-center">Don’t miss out!</h1>
       <p className="text-base leading-normal text-center text-white mt-6">
