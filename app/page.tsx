0fdde77606050ac3ef78fc76ba2add391c341518
brooklyn-rgb <brooklynstore151@gmail.com>
2024-10-13 18:26:53 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 import Image from "next/image";
 import Bnr from "./components/Bnr";
-import Hero from "./components/Hero";
 import About from "./pages/about/page";
 import Team from "./pages/team/page";
 import About2 from "./pages/about2/page";
 import Video from "./pages/video/page";
+import { cookies } from 'next/headers'
 
 
 export default function Home() {
+  const cookieStore = cookies()
+  const theme = cookieStore.get('theme')
   return (
     <div>
-      <Hero />
+      
       <Team />
     <About2 />
     <Video />
